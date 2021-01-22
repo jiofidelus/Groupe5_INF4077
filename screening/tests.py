aa91827dcea9696563642cf934a9c7a0bde5e119
donaldoempire1999 <=>
2021-01-22 13:48:38 +0100

    Integration django Selenium

--- screening/tests.py
+++ screening/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from unittest import skipUnless
 
-# Create your tests here.
+import selenium
+from django.conf import settings
+from django_selenium_test import SeleniumTestCase, PageElement
+from selenium.webdriver.common.by import By
+
+
+@skipUnless(getattr(settings, 'SELENIUM_WEBDRIVERS', False),
+            "Selenium is unconfigured")
+class HelloTestCase(SeleniumTestCase):
+    heading_earth = PageElement(By.ID, 'earth')
+    heading_world = PageElement(By.ID, 'world')
+    button = PageElement(By.CSS_SELECTOR, 'button')
+
+    def test_toggle(self):
+        # Visit the page
+        self.selenium.get(self.live_server_url)
+
+        # Check that the earth heading is visible
+        self.assertTrue(self.heading_earth.is_displayed())
+        self.assertFalse(self.heading_world.is_displayed())
+
+        # Toggle and check the new condition
+        self.button.click()
+        self.heading_world.wait_until_is_displayed()
+        self.assertFalse(self.heading_earth.is_displayed())
+        self.assertTrue(self.heading_world.is_displayed())
+
+        # Toggle again and re-check
+        self.button.click()
+        self.heading_earth.wait_until_is_displayed()
+        self.assertTrue(self.heading_earth.is_displayed())
+        self.assertFalse(self.heading_world.is_displayed())
